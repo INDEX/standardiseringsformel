3166c0ca0f8ba137e26572ea2ae9b01332680dbd
Oscar <61657799+INDEX@users.noreply.github.com>
2020-04-15 12:03:13 +0200

    Filer uploaded - forhåbentlig korrekt

---
+++ mejerist.js
@@ -0,0 +1,34 @@
+var formel = document.getElementById("indtast");
+var over1 = document.getElementById("over");
+var under1 = document.getElementById("fdn");
+var endelig1 = document.getElementById("endelig");
+var finale1 = document.getElementById("finale");
+var finale3 = document.getElementById("finale2");
+
+document.getElementById("submit--js").addEventListener("click", function(event) {
+let kgMilk = document.getElementById("kgMilk--js").value;
+let fatPercent = document.getElementById("fatPercent--js").value;
+let skmFat = document.getElementById("skmFat--js").value;
+let sFat = document.getElementById("sFat--js").value;
+function totalS() {
+    return kgMilk * (fatPercent - skmFat) / (sFat - skmFat);
+  };
+  function totalSkm () {
+    return kgMilk - totalS();
+  };
+    indtast.innerHTML += `Du skal bruge ${totalS().toFixed(2)} kg. sødmælk/fløde og 
+    ${totalSkm().toFixed(2)} kg. skummetmælk`;
+    document.getElementById("regnestykke").style.display = "block";
+    document.getElementById("regnestykke2").style.display = "block";
+    document.getElementById("igen").style.display = "block";
+    document.getElementById("slet").style.display = "none";
+    over.innerHTML +=`${kgMilk} x (${fatPercent} - ${skmFat})`;
+    fdn.innerHTML += `(${sFat} - ${skmFat})`;
+    endelig.innerHTML += `${totalS().toFixed(2)} kg.`
+    finale.innerHTML += `${kgMilk} - ${totalS().toFixed(2)}`;
+    finale2.innerHTML += `${totalSkm().toFixed(2)} kg.`
+
+
+});
+
+
