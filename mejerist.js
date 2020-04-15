f32d54934f4e6437e7ea042230bdfa7a29bcc084
Oscar <61657799+INDEX@users.noreply.github.com>
2020-04-15 20:50:38 +0200

    Update mejerist.js

--- mejerist.js
+++ mejerist.js
@@ -11,7 +11,7 @@ let fatPercent = document.getElementById("fatPercent--js").value;
 let skmFat = document.getElementById("skmFat--js").value;
 let sFat = document.getElementById("sFat--js").value;
 if (fatPercent > sFat || fatPercent < skmFat) {
-    alert("Husk at den ønskede fedtprocent skal ligge mellem skummetmælk og sødmælk/fløde");
+    alert("Husk at den ønskede fedtprocent skal ligge mellem fedtprocenten for skummetmælk og sødmælk/fløde");
 }
     function totalS() {
     return kgMilk * (fatPercent - skmFat) / (sFat - skmFat);
